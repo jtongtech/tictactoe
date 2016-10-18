cbd1fc328ac5707317906c77293f74415073b6ba
JTong <eldertong@gmail.com>
2016-10-18 15:39:00 -0400

    working with jake

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 #make a board print to the screen
-3.times do
-puts "| | | |"
+def tic(*input)
+    positions = ["", "X", "", "", "", "", "", "", "", ""]
+    puts " #{positions[1]} | #{positions[2]} | #{positions[3]} "
+    puts "-----------"
+    puts " #{positions[4]} | #{positions[5]} | #{positions[6]} "
+    puts "-----------"
+    puts " #{positions[7]} | #{positions[8]} | #{positions[9]} "
 end
 
+puts tic
 
 #array of strings where each string is an input
 #hash where key is place on board and value is x or o or " "
