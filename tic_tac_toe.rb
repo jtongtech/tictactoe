9b5a7c75a5060204af533b210fc02c09c1a7d6a4
JTong <eldertong@gmail.com>
2016-10-20 14:13:27 -0400

    working on this with adam

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -44,11 +44,15 @@
 # # end
 
 class TicTacToeBoard #naming the class
-    attr_reader :board
+    attr_reader :board, :player1
     
     def initialize
         @board = Board.new
     end
+
+    def player_names
+        puts "Player 1 has entered the game!  Please enter your username" 
+        @player_1.name = gets.chomp
 
     class Board
         attr_reader :board, :empty_square
@@ -58,9 +62,9 @@ class TicTacToeBoard #naming the class
             @board = Array.new(9, @empty_square) #makes an array of 9 " "'s
         end
         
-        def print_board
+        def print_board #prints 3 rows in 3 columns with | between the columns
             puts "\n"
-            @board.each_slice(3) {|row| puts row.join(' | ')} 
+            @board.each_slice(3) {|row| puts row.join(' | ')}  #breaks @board into sections of 3 and puts a new line between each section
             puts "\n"
         end
 end
