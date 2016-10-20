b47f5ea9393e3777cca85a49f5a9a466dda6cc08
JTong <eldertong@gmail.com>
2016-10-20 13:02:49 -0400

    reach out to jacob who advises scrapping code and starting with classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,16 +9,32 @@ end
 
 puts print_board
 
-puts "What is your input?"
-answer = gets
-puts "answer = #{answer}"
-
-# def monkey(answer)
+def get_input
+    puts "What is your input?"
+    answer = gets
+    puts "answer = #{answer}"
     position = answer[0]
     player = answer[1]
-# end
-puts "position = #{position}"
-puts "player = #{player}"
+    puts "position = #{position}"
+    puts "player = #{player}"
+end
+
+def update_board1
+    positions = ["", "X", "", "", "", "", "", "", "", ""]
+    puts " #{positions[1]} | #{positions[2]} | #{positions[3]} "
+    puts "-----------"
+    puts " #{positions[4]} | #{positions[5]} | #{positions[6]} "
+    puts "-----------"
+    puts " #{positions[7]} | #{positions[8]} | #{positions[9]} "
+end
+
+puts get_input
+
+if get_input == 1
+    puts update_board1
+end
+
+
 # puts player
 
 #     puts firstmove
