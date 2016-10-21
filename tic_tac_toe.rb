682b733b1524378a131cd17978071a997d5ccf8f
JTong <eldertong@gmail.com>
2016-10-21 12:11:33 -0400

    displaying welcome message but not printing board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,21 +1,33 @@
-class TicTacToeBoard #naming the class
-    attr_reader :board, :player_1, :player_2
+class Game #names the class
+    attr_reader :grid, :player_1, :player_2 #sets attributes for the class
     
     def initialize
-        @board = Board.new
+        @grid = Board.new
         @player_1 = Player.new
         @player_2 = Player.new
+    end
+
+    def welcome
+        puts "\n"
+        puts "****************************************"
+        puts "* Welcome to Jeremy's Tic-Tac-Toe Game *"
+        puts "****************************************"
+    end
+
+    def play_game
+        welcome
+        player_names
     end
 
     def player_names
         puts "Player 1 has entered the game!  Please enter your username" 
         @player_1.name = gets.chomp #collects user input of name and removed last element of string
-        puts "Welcome #{@player_1}, you will be player X"
+        puts "Welcome #{@player_1.name}, you will be player X"
         puts "\n"
         @player_1.symbol = 'X' #sets player_1 symbol to X
         puts "Player 2 has entered the game!  Please enter your username" 
         @player_2.name = gets.chomp #collects user input of name and removed last element of string
-        puts "Welcome #{@player_2}, you will be player O"
+        puts "Welcome #{@player_2.name}, you will be player O"
         puts "\n"
         @player_2.symbol = 'O' #sets player_1 symbol to O
         puts "\n"
@@ -29,14 +41,13 @@ class TicTacToeBoard #naming the class
             @board = Array.new(9, @empty_square) #makes an array of 9 " "'s
         end
         
-        def print_board #prints 3 rows in 3 columns with | between the columns
+        def print_grid #prints 3 rows in 3 columns with | between the columns
             puts "\n"
             @board.each_slice(3) {|row| puts row.join(' | ')}  #breaks @board into sections of 3 and puts a new line between each section
             puts "\n"
         end
     end
     Player = Struct.new(:name, :symbol)
-
-
-TicTacToeBoard.new.player_names.board.print_board
 end
+
+Game.new.play_game.print_grid
