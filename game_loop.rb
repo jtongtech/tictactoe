63f070751e45869ae80a2d8f9e45350cf89d8efd
JTong <eldertong@gmail.com>
2016-10-25 16:52:37 -0400

    homework?

--- game_loop.rb
+++ game_loop.rb
@@ -5,3 +5,9 @@ game = ConsoleGame.new(Human.new("X"), (Human.new("O")))
 game.print_board
 
 move = game.get_move
+
+game.make_move(move)
+
+game.print_board
+
+#loop this and make it use the other methods
