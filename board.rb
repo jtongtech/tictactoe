d94ff10e0ce758a59ea0e8b5a5400579fae537a9
JTong <eldertong@gmail.com>
2016-10-27 13:22:15 -0400

    can play game but no winner and no tie is announced

--- board.rb
+++ board.rb
@@ -4,7 +4,7 @@
         def initialize
             @empty_square = "" #if the square has not been played it is empty
             #@board = Array.new(9, @empty_square) #makes an array of 9 " "'s
-            @position_board = Array.new(9, "")
+            @position_board = Array.new(10, "")
         end
         
         def update(position, symbol) #this is how to space is updated
