040e2022e03cbc745835cc8d5308178c5f50fbef
JTong <eldertong@gmail.com>
2016-10-27 18:55:38 -0400

    added jake's code as comment

--- game_loop.rb
+++ game_loop.rb
@@ -11,3 +11,16 @@ until game.game_over?
 end
 
 #loop this and make it use the other methods
+
+# require_relative "console_game.rb"
+
+# game = Game.new(Human.new("x"), Human.new("o"))
+
+# until game.game_over?
+# # until game.board.winner?(game.current_player.marker) || game.board.full_board?
+#     game.change_player
+#     game.print_board
+#     move = game.get_move
+#     game.make_move(move)
+#     game.print_board
+# end
