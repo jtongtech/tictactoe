69b426c33c9bd61c7bf0796e5e9896362728bcf7
JTong <eldertong@gmail.com>
2016-10-27 19:44:50 -0400

    working if comments are right

--- game_loop.rb
+++ game_loop.rb
@@ -10,17 +10,19 @@ until game.game_over?
     game.change_player
 end
 
-#loop this and make it use the other methods
-
 # require_relative "console_game.rb"
 
 # game = Game.new(Human.new("x"), Human.new("o"))
 
 # until game.game_over?
-# # until game.board.winner?(game.current_player.marker) || game.board.full_board?
 #     game.change_player
 #     game.print_board
 #     move = game.get_move
 #     game.make_move(move)
 #     game.print_board
 # end
+
+# game.end_message
+
+
+# # Tie 142563789
