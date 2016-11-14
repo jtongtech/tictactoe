5544c45fbc67eeb9c987ddd2e9fb2bd0f87307da
JTong <eldertong@gmail.com>
2016-11-14 15:11:38 -0500

    starting to work on making a web app for ttt

---
+++ public/js/auto_box.js
@@ -0,0 +1,13 @@
+    function myFunction() {
+        var x = document.getElementById("myInput").value;
+        document.getElementById("demo").innerHTML = "Enter their names in the boxes below:<br>";
+        for (var i = 1; i <= x; i++) {
+            var input = document.createElement("input");
+            input.type = "text";
+            input.name = "text" + i;
+            input.id = "marv";
+            input.required = true;
+            input.placeholder = "Enter Name"
+            document.getElementById("demo").appendChild(input).value;
+        }
+    }
