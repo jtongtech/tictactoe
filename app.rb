a2872324ab09f7c2aad18ec271325d5228a5b0d0
JTong <eldertong@gmail.com>
2016-11-28 12:56:18 -0500

    comparing my code to adams

--- app.rb
+++ app.rb
@@ -32,32 +32,32 @@ end
 post '/choose_opponent' do
     player_2 = params[:player_2]
 
-    if player_2 == "human"
-        session[:player_2] = Console_human.new("O")
-        
-        erb :player_2_name, :layout => :home_layout, :locals => { :board => session[:board].board_positions }
-        session[:player_2_name] = session[:player_2]
+	if player_2 == "human"
+		session[:player_2] = Console_human.new("O")
+		
+		erb :player_2_name, :layout => :home_layout, :locals => { :board => session[:board].board_positions }
+		session[:player_2_name] = session[:player_2]
 
-       redirect '/player_2_name'
+        redirect '/player_2_name'
 
-    elsif player_2 == "sequential_ai"
-        session[:player_2] = SequentialAI.new("O")
-        session[:player_2_name] = "Easy"
-        
-        redirect '/get_move'
+	elsif player_2 == "sequential_ai"
+		session[:player_2] = SequentialAI.new("O")
+		session[:player_2_name] = "Easy"
+		
+		redirect '/get_move'
 
-    elsif player_2 == "random_ai"
-        session[:player_2] = RandomAI.new("O")
-        session[:player_2_name] = "Medium"
+	elsif player_2 == "random_ai"
+		session[:player_2] = RandomAI.new("O")
+		session[:player_2_name] = "Medium"
 
-        redirect '/get_move'
+		redirect '/get_move'
 
-    else player_2 == "unbeatable_ai"
-        session[:player_2] = UnbeatableAI.new("O")
-        session[:player_2_name] = "Hard"
-        
-        redirect '/get_move'
-    end
+	else player_2 == "unbeatable_ai"
+		session[:player_2] = UnbeatableAI.new("O")
+		session[:player_2_name] = "Hard"
+		
+		redirect '/get_move'
+	end
 end
 
 get '/player_2_name' do
